23dc6c4ab26a87cb226baefc528fff5e01aed2ab
tatarinovich.a <tatarinovich.a@if-dev12.mara.local>
2017-10-19 16:58:46 +0300

    Initial version Spring Boots
    Change-Id: Id074625948bdae03c5004bb4d422b992034a2c2d

---
+++ product-rating/src/main/java/by/productrating/ProductRatingApplication.java
@@ -0,0 +1,14 @@
+package by.productrating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+public class ProductRatingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductRatingApplication.class, args);
+	}
+}
